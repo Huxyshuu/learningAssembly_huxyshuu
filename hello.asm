ae7be277c674e7d590f20b21420b8bb1bc5e3f45
Hugo Tamm <hugotamm@hotmail.com>
2024-02-12 15:55:51 +0200

    Lesson 5 complete

--- hello.asm
+++ hello.asm
@@ -1,36 +1,19 @@
+%include "functions.asm"
+
 SECTION .data
-	msg		db "Yo this assembly is neeto!", 0x0A
+msg1	db		"Yo this assembly is neeto!", 0x0A
+msg2	db		"Yo recycling in NASM", 0x0A
 
 SECTION .text
 global _start
 
 _start:
-	mov		eax, msg
-	call 	strlen
+	mov		eax, msg1
+	call 	sprint
 
-	mov 	edx, eax
-	mov 	ecx, msg
-	mov 	ebx, 1
-	mov 	eax, 4
-	int 	0x80
-
-	mov 	eax, 1 		; return 0 status on exit - "No errors"
-	mov 	ebx, 0 		; invoke SYS_EXIT (kernel opcode 1)
-	int 	0x80
-
-strlen:
-	push 	ebx
-	mov 	ebx, eax
-
-nextchar:
-	cmp 	byte[eax], 0 ; compare the byte pointed to by EAX at this address against zero (Zero is an end of string delimiter)
-	jz 		finished
-	inc 	eax
-	jmp 	nextchar
-
-finished:
-	sub 	eax, ebx
-	pop 	ebx
-	ret
+	mov 	eax, msg2
+	call 	sprint
+	
+	call 	quit
 
 
